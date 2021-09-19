92e66d62e17022fd8db77a5505753fec6f2b4d85
Joshua Berliner <joshuaberliner@gmail.com>
2021-09-19 16:21:34 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    //debugger;
+}
+
+function writeCards (names, giftType = 'surprise'){
+    const namesList = [];
+    let i = 0;
+    while (i < names.length){
+        namesList.push(`Thank you, ${names[i]}, for the wonderful ${giftType} gift!`)
+        i++;
+    }
+    return namesList;
+
+}
+
+function countDown (number){
+    if (number >= 0){
+        while (number >= 0){
+            console.log(number);
+            number--;
+        }
+    }
+    
+}
